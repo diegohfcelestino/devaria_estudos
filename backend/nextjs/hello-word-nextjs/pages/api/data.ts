cdde18337ed3b1e48832306e7b5cf5df916f8e98
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-22 14:35:24 -0300

    Criando primeiro projeto com next

---
+++ backend/nextjs/hello-word-nextjs/pages/api/data.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import moment from "moment";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ data: moment().format("DD/MM/yyy HH:mm:ss") });
+};
---
+++ backend/nextjs/hello-word-nextjs/pages/api/hello.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+  idade: number;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req.method);
+  res.status(200).json({ name: "Diego Ferreira", idade: 35 });
+}
