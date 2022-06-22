7deea765b0dc6d4bab2460b848399efca3773acf
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-22 12:20:47 -0300

    Trabalhando com arrays e excessões

---
+++ backend/nodejs/arrays-execoes/app.js
@@ -0,0 +1,30 @@
+
+try {
+  const listaDeProdutosDisponiveis = [
+    "Pao",
+    "Leite",
+    "Café",
+    "Laranja",
+    "Macarrão",
+    "Sabonete",
+    "Detergente",
+  ];
+
+  const listaDeArgumentos = process.argv.slice(2);
+
+  const listaDeProdutosSolicitadosDisponiveis = listaDeProdutosDisponiveis.filter(produto => {
+    return listaDeArgumentos.find(argumento => argumento === produto);
+  });
+  listaDeProdutosSolicitadosDisponiveis.forEach(produto => console.log(`Este produto nos temos: ${produto}`));
+
+  const listaDeProdutosSolicitadosNaoDisponiveis = listaDeArgumentos.filter(argumento => {
+    return !listaDeProdutosDisponiveis.find(produto => produto === argumento);
+  });
+  listaDeProdutosSolicitadosNaoDisponiveis.forEach(argumento => console.log(`Este produto nós não temos: ${argumento}`));
+
+  listaDeProdutosDisponiveis.sort();
+  listaDeProdutosDisponiveis.forEach(produto => console.log(`Este produto está disponível: ${produto}`));
+} catch (e) {
+  console.log("Não foi possível executar pedidos de compra");
+}
+
