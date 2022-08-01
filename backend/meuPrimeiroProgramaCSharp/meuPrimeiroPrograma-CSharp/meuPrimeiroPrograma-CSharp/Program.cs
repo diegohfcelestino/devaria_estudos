0fb51784f3d4a6f5bc9df468e1d7c41fc2e44f80
diegohfcelestino <diegohfcelestino@gmail.com>
2022-08-01 09:22:09 -0300

    Iniciando estudos com c#

---
+++ backend/meuPrimeiroProgramaCSharp/meuPrimeiroPrograma-CSharp/meuPrimeiroPrograma-CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿const string textoOlaMundo = "Olá mundo, esse é meu primeiro programa c#, feito por ";
+string nomeCompleto;
+int idade;
+
+
+Console.WriteLine("Qual seu nome completo?");
+nomeCompleto = Console.ReadLine().ToString();
+Console.WriteLine("Qual sua idade?");
+idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(textoOlaMundo + nomeCompleto + " que tem " + idade + " anos");
